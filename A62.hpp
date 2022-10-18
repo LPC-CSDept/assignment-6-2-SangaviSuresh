f9d388d2ae4b8cd55ba19d204bb5ce92ae39cafb
SangaviSuresh <111937090+SangaviSuresh@users.noreply.github.com>
2022-10-18 19:52:52 +0000

    commit

--- A62.hpp
+++ A62.hpp
@@ -1,5 +1,3 @@
-//
-
 #include <iomanip>
 #include <iostream>
 using namespace std;
@@ -7,14 +5,24 @@ using namespace std;
 // complete two functions
 double nthpowerp(int n)
 {
-        // return n_th power number
-        //complete all code
+//find the nth power of result
+    double result = 1;
+        for(int i = 0; i < n; i++) {
+                result = result * 2;
+        }
+        return result;
 }
 double nthpowern(int n)
 {
-        // return n_th power number
+// find nth power of result
+    double result = 1;
+        for(int i = 0; i > n; i--) {
+                result = result/2;
+        }
+        return result;
 }
 void printout(double result)
 {
-        cout << "The result is " << setw(10) << fixed << setprecision(5) << result << endl;
-}
+// output results
+    cout << "The result is " << setw(10) << fixed << setprecision(5) << result << endl;
+} 
